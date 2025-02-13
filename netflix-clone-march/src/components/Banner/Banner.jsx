ede572f696035720061d51a34ef2670dbd4403c5
ezzy-sam <gebremichaelsamson78@gmail.com>
2025-02-13 16:29:13 +0000

    row

--- netflix-clone-march/src/components/Banner/Banner.jsx
+++ netflix-clone-march/src/components/Banner/Banner.jsx
@@ -11,8 +11,6 @@ const Banner = ()=>  {
             try {
                 const request = await axios.get(requests.fetchNetflixOriginals);
                 console.log(request);
-                
-
                 setMovie(
                   request.data.results[
                     Math.floor(Math.random() * request.data.results.length)
@@ -39,34 +37,26 @@ const Banner = ()=>  {
 
 
   return (
-    <div className='banner'
-    style={{
-
-
-
-
-
-
+    <div
+      className="banner"
+      style={{
         backgroundSize: "cover",
-        
-
         backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
         backgroundPosition: "center",
-        backgroundRepeat: "no-repeat"
-    }}
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <div className="banner_contents">
         <h1 className="banner_title">
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
-        <div className="banner_buttons">
+        <div className="banner_button">
           <button className="banner_button play">Play</button>
           <button className="banner_button">My List</button>
         </div>
-        <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
-
-        <div className="banner_fadeButtom"></div>
       </div>
+      <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+      <div className="banner_fadeButtom"></div>
     </div>
   );
 }
--- netflix-clone-march/src/pages/Home/Home.jsx
+++ netflix-clone-march/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Banner from '../../components/Banner/Banner'
+import RowList from '../../components/Rows/RowList/RowList'
 
 
 import Footer from '../../components/Footer/Footer'
@@ -8,9 +9,10 @@ import Footer from '../../components/Footer/Footer'
 function Home() {
   return (
     <>
-        <Header/>
-        <Banner/>
-        <Footer/>
+        <Header />
+        <Banner />
+        <RowList />
+        <Footer />
     </>
   )
 }

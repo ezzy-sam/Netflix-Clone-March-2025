d2973a41c2eea2bc7554e638a4e6224173cd04e1
ezzy-sam <gebremichaelsamson78@gmail.com>
2025-02-14 12:30:02 +0000

    improved diploy

--- netflix-clone-march/src/components/Rows/Row/Row.jsx
+++ netflix-clone-march/src/components/Rows/Row/Row.jsx
@@ -1,83 +1,85 @@
-import React, { useEffect, useState } from 'react'
-import './row.css';
+import React, { useEffect, useState } from "react";
+import "./row.css";
 import axios from "../../../utils/axios";
-import movieTrailer from 'movie-trailer';
-import YouTube from 'react-youtube';
-
-
-
-
-
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
-const [movies, setMovie] = useState([]);
-const [trailerUrl, setTrailerUrl] = useState("");
+  const [movies, setMovie] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-const base_url = "https://image.tmdb.org/t/p/original";
+  const base_url = "https://image.tmdb.org/t/p/original";
 
-useEffect(() => {
-  (async () => {
-    try {
-      console.log(fetchUrl);
-      const request = await axios.get(fetchUrl);
-      console.log(request);
-      setMovie(request.data.results);
-    } catch (error) {
-      console.log("error", error);
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(fetchUrl);
+        setMovie(request.data.results);
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, [fetchUrl]);
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          if (url) {
+            const urlParams = new URLSearchParams(new URL(url).search);
+            setTrailerUrl(urlParams.get("v"));
+          } else {
+            alert("Trailer not found!");
+          }
+        })
+        .catch((error) => {
+          console.log("Trailer fetch error:", error);
+          alert("Failed to load trailer!");
+        });
     }
-  })();
-}, [fetchUrl]);
+  };
 
-const handleClick = (movie) => {
-  if (trailerUrl){
-    setTrailerUrl('')
-  }else {
-    movieTrailer(movie?.title || movie?.name || movie?.original_name)
-    .then((url) => {
-      console.log(url);
-      const urlParams = new URLSearchParams(new URL(url).search)
-      console.log(urlParams)
-      console.log(urlParams.get('v'))
-      setTrailerUrl(urlParams.get('v'))
-      
-      
-      
-    })
-  }
-
-
-}
-
-const opts = {
-  heigth: '390',
-  width: "100%",
-  playerVars: {
-    autoplay:1,
-  },
-}
-
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   return (
-    <div className='row'>
+    <div className="row">
       <h4>{title}</h4>
-      <div className='row_posters'>
-        {movies?.map((movie, index)=>(
+      <div className="row_posters">
+        {movies?.map((movie, index) => (
           <img
-
-          onClick={() => handleClick(movie)}
-          key={index} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            onClick={() => handleClick(movie)}
+            key={index}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
           />
         ))}
-
       </div>
-      <div style={{ padding: '40px'}}>
-        {trailerUrl && <YouTube videoID={trailerUrl} opts={opts}/>}
-
+      <div style={{ padding: "40px" }}>
+        {trailerUrl && (
+          <YouTube
+            videoId={trailerUrl}
+            opts={opts}
+            onError={(e) => {
+              console.log("YouTube error:", e);
+              setTrailerUrl("");
+              alert("Video unavailable!");
+            }}
+          />
+        )}
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default Row
+export default Row;
